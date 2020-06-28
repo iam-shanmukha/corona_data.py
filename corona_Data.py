8b6c5ad29dda0083d880ef372c2d2971ac7cc4b3
iam-shanmukha <vishnushanmukha@gmail.com>
2020-06-28 19:42:26 +0530

    added files

--- corona_Data.py
+++ corona_Data.py
@@ -2,8 +2,12 @@ from covid import Covid
 import time
 import tweepy
 from os import *
+import datetime
+
 covid = Covid()
 India_cases = covid.get_status_by_country_id(27)
+
+delta_hour = 0
 
 ############################Twitter########################
 consumer_key =environ['consumer_key']
@@ -14,13 +18,18 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-for param,val in India_cases.items():
-	#print("{} : {}".format(param,val))
-	#print(val)
-	val = str(val)
+while:
+    now_hour = datetime.datetime.now().hour
+    if delta_hour != now_hour:
+		for param,val in India_cases.items():
+			#print("{} : {}".format(param,val))
+			#print(val)
+			val = str(val)
 
-t=time.ctime(int(val[:10]))
+		t=time.ctime(int(val[:10]))
 
-print(f"Last Update : {t}" + "\n".join("{} : \t{}".format(k, v) for k, v in India_cases.items()))
-print(t)
-api.update_status(status=f"Last Update : {t}" + "\n"+ "\n".join("{} : \t{}".format(k, v) for k, v in India_cases.items()))
+		#print(f"Last Update : {t}" + "\n".join("{} : \t{}".format(k, v) for k, v in India_cases.items()))
+		#print(t)
+		api.update_status(status=f"Last Update : {t}" + "\n"+ "\n".join("{} : \t{}".format(k, v) for k, v in India_cases.items()))
+	delta_hour = now_hour
+	time.sleep(60)
