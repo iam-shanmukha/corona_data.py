0e3eb233c0695fbc6a2683eda3b236877b625a03
Shanmukha Vishnu <50124557+iam-shanmukha@users.noreply.github.com>
2020-12-25 22:18:13 +0530

    Update corona_Data.py

--- corona_Data.py
+++ corona_Data.py
@@ -17,7 +17,7 @@ while(1):
 		covid = Covid(source="worldometers")
 		India_cases = covid.get_status_by_country_name("india")
 		print("\n"+ "\n".join("{} : \t{}".format(k, v) for k, v in India_cases.items() if not k.startswith(('population','total')))+"\n#IndiaFightsCorona")
-		api.update_status(status="\n".join("{} : \t{}".format(k, v) for k, v in India_cases.items() if not k.startswith(('population','total')))+"\n#IndiaFightsCorona"))
+		api.update_status(status="\n".join("{} : \t{}".format(k, v) for k, v in India_cases.items() if not k.startswith(('population','total')))+"\n#IndiaFightsCorona")
 		print("Tweeted")
 	except tweepy.TweepError as e:
 		print("Duplicate")
